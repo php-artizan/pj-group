3a3d687b5c3f495d81c05fc9288847e2ff3ede7c
ROMALANWER <Romailanwer7@gmail.com>
2024-07-06 10:49:14 +0500

    index.php
    index.php home page

---
+++ include/head.php
@@ -0,0 +1,16 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>Zameen</title>
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
+
+
+	<!-- Custom Color Option -->
+	<link href="assets/css/colors.css" rel="stylesheet">
+
+</head>
